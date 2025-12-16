db85efd4f410dc7d9f34770570083193d06008e4
Nestor <nesto.castro@zeeni.com>
2025-12-15 18:21:45 -0800

    enable ecommerce panel for users on production version

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $panel->getId() === 'ecommerce';
+    }
 
     /**
      * The attributes that are mass assignable.
